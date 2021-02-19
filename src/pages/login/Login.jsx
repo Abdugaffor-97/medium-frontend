1a88ebd531ebf3ef7fe5cc723976c9d0ec216898
Abdugaffor <abdurahimov.97@list.ru>
2021-02-19 12:59:27 +0500

    Style

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import  "./style.scss";
 
 const Login = () => {
   return (
-    <div>
-      <a
-        href={process.env.REACT_APP_API_URL + "/users/google-redirect"}
-        className="btn"
-      >
-        Login With Google
-      </a>
+
+    <div className='login-wrapper'>
+      <div className='login'>
+        <a
+          href={process.env.REACT_APP_API_URL + "/users/google-redirect"}
+          className="btn"
+        >
+          Login With Google
+        </a>
+      </div>
     </div>
   );
 };
